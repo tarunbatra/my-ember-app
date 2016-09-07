185a2f95d5b4618c72d31c95b2efee9a211c2d94
Tarun Batra <tarun.batra00@gmail.com>
2016-09-08 00:18:39 +0530

    Adds basic sorting logic

--- app/components/data-list.js
+++ app/components/data-list.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 
 export default Ember.Component.extend({
+  actions: {
+    toggleSort: function (params) {
+      let sortedList = this.get('list').sortBy(params);
+      this.set('list', sortedList);
+    }
+  }
 });
