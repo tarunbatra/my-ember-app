3927fc67312ae5e7c91f041572f8a83193357c48
Tarun Batra <tarun.batra00@gmail.com>
2016-09-08 02:32:27 +0530

    Adds price range filter and refactoring

--- app/components/data-filter.js
+++ app/components/data-filter.js
@@ -12,23 +12,48 @@ let _generateSelectBoxOptions = function (list) {
   this.set('options', options);
 };
 
+let _filterByRelationshipManager = function (list, query) {
+  return list.filter(function (listItem) {
+    let rshipManager = listItem.get('relationshipManager');
+    return rshipManager.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+  });
+};
+
+let _extractNumberFromCurrency = function (currency) {
+  return Number(currency.replace(/[^0-9\.]+/g,""));
+};
+
+let _restrictListByRevenue = function (list, minValue, maxValue) {
+  return list.filter(function(listItem) {
+    var revenue = _extractNumberFromCurrency(listItem.get('revenueGenerated'));
+    return (revenue >= (minValue || 0) && revenue <= (maxValue || Number.MAX_SAFE_INTEGER));
+  });
+};
+
 export default Ember.Component.extend({
+
   init: function() {
     this._super(...arguments);
     let list = this.get('list');
     this.set('filteredList', list);
     _generateSelectBoxOptions.call(this, list);
   },
+
   actions: {
-    filter: function(params) {
-      let data = this.get('list');
+    filter: function(searchText) {
 
+      let list = this.get('list');
+      console.log('---',searchText);
       // Filtering logic
-      let filteredList = data.filter(function (dataItem) {
-        let rshipManager = dataItem.get('relationshipManager');
+      let filteredList = searchText ? _filterByRelationshipManager(list, searchText): list;
 
-        return rshipManager.toLowerCase().indexOf(params.toLowerCase()) !== -1;
-      });
+      let minValue = this.get('min');
+      let maxValue = this.get('max');
+
+      if (minValue || maxValue) {
+        filteredList = _restrictListByRevenue(filteredList, minValue, maxValue);
+      }
+
       this.set('filteredList', filteredList);
     }
   }
